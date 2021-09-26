e9ad5ae5469a3e433379cbaa25f01459ea76c732
brunoczim <brunoczim@gmail.com>
2021-09-26 11:24:10 -0300

    initial code for innerpedia

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! This crate gives facilities to create an encyclopedia. Besides paths and
+//! pages as a whole, this crate provides several tools on the page contents.
+
+pub mod location;
+pub mod component;
+pub mod page;
+pub mod site;
+pub mod fmt;
